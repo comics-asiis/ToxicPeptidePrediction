4ec040ae61569ea725c3b775fd76f8d7a34985af
comics-asiis <74765435+comics-asiis@users.noreply.github.com>
2023-09-26 22:23:26 +0800

    Add files via upload

---
+++ program_resource/toxteller.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import errno
+import modelwizard
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    now1 = datetime.now()
+    time1 = now1.strftime("%Y-%m-%d@%H%M%S")
+    print('Time:')
+    print(time1)
+
+    if len(sys.argv) > 1:
+        # argv[1] is the path of input fasta
+        input_fastapath = sys.argv[1]
+        if not os.path.isfile(input_fastapath):
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), input_fastapath)
+        print('Fasta path:', input_fastapath)
+        print('Output directory:', os.path.dirname(os.path.realpath(__file__)))
+        output_filename = modelwizard.load_4models_predict(input_fastapath)
+        print('Result file name:', output_filename)
+
+    else:
+        print('No input detected')
+
+    now2 = datetime.now()
+    time2 = now2.strftime("%Y-%m-%d@%H%M%S")
+    print('\nTime when finished:')
+    print(time2)
