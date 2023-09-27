a9a4e19bf4cc22e85782cded9735c724a2735079
comics-asiis <74765435+comics-asiis@users.noreply.github.com>
2023-09-27 16:52:14 +0800

    Update modelwizard.py

--- program_resource/modelwizard.py
+++ program_resource/modelwizard.py
@@ -7,15 +7,15 @@ from datetime import datetime
 import accessdata
 import extractfeature
 
-
+""" The main body of the process for toxicity prediction """
 def load_4models_predict(input_fastapath, ll_seqlen=10, ul_seqlen=50, given_features=None, output_dir=None, max_seq_num=10000):
     pred_seq_di, notproc_seq_di = accessdata.readfasta(input_fastapath, ul_seqlen=ul_seqlen, ll_seqlen=ll_seqlen)
     if len(pred_seq_di) > max_seq_num:
         print('Not allowed to process more than ' + str(max_seq_num) + 'sequences')
         return "No result"
-
+    
+    # Load 4 ML models: LR, SVM, RF, & XGBoost from pickle files (4 pairs of models and scalers)
     current_dir = os.getcwd()
-    # Load 4 ML models: LR, SVM, RF, & XGBoost from pickle files (4 pairs of model files and scaler files)
     model_lr = pickle.load(open(current_dir + '/model_lr.pkl', 'rb'))
     scaler_lr = pickle.load(open(current_dir + '/scaler_lr.pkl', 'rb'))
 
@@ -33,21 +33,19 @@ def load_4models_predict(input_fastapath, ll_seqlen=10, ul_seqlen=50, given_feat
                        'Hyd2', 'Hydro', 'FEtmh', 'Pol2']
     considered_features = feat_adnc_pcp13 if given_features is None else given_features
 
-    pred_lr, prob_lr = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_lr, scaler_lr)
-    pred_svm, prob_svm = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_svm, scaler_svm)
-    pred_rf, prob_rf = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_rf, scaler_rf)
-    pred_xgb, prob_xgb = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_xgb, scaler_xgb)
+    pred_lr = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_lr, scaler_lr)
+    pred_svm = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_svm, scaler_svm)
+    pred_rf = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_rf, scaler_rf)
+    pred_xgb = EncodeTestDataAndPredict(pred_seq_di, considered_features, model_xgb, scaler_xgb)
 
+    # Create the dataframe for the prediction results
     df_pred_result = extractfeature.GenerateInitialSequenceDataframe(pred_seq_di, islabeled=False)
     df_pred_result['LR prediction'] = pred_lr
     df_pred_result['SVM prediction'] = pred_svm
     df_pred_result['RF prediction'] = pred_rf
     df_pred_result['XGBoost prediction'] = pred_xgb
-    df_pred_result['LR prediction probability'] = prob_lr[:,1]
-    df_pred_result['SVM prediction probability'] = prob_svm[:,1]
-    df_pred_result['RF prediction probability'] = prob_rf[:,1]
-    df_pred_result['XGBoost prediction probability'] = prob_xgb[:,1]
 
+    # Export the prediction results to a CSV file
     result_csv_name = input_fastapath + '.csv'
     result_csv_path = result_csv_name if output_dir is None else output_dir + '/' + result_csv_name
     df_pred_result.to_csv(result_csv_path)
@@ -58,7 +56,7 @@ def EncodeTestDataAndPredict(seq_di, considered_features, given_model, given_sca
     df_data = extractfeature.GenerateFeatureTableGivenSeqDiAndFeatureLi(seq_di, considered_features, islabeled=False) 
     x_data = df_data[df_data.columns[~df_data.columns.isin(['Name', 'TrueLabel', 'Sequence', 'Length'])]]
     
-	# Features belonging to physicochemical properties need to be normalized with given_scaler
+    # Features belonging to physicochemical properties need to be normalized using given_scaler
     pcp13_feats = ['pKa1', 'pKa2', 'Vol', 'VSC', 'SA', 'pI', 'Chg', 'NCIS', 'Hyd1', 'Hyd2', 'Hydro', 'FEtmh', 'Pol2']
     feats_needstandardize = [x for x in pcp13_feats if x in x_data.columns]  
     df_needstddz = x_data[feats_needstandardize]
@@ -67,8 +65,7 @@ def EncodeTestDataAndPredict(seq_di, considered_features, given_model, given_sca
     x_data_todrop.drop(labels=feats_needstandardize, axis="columns", inplace=True)
     x_data = pd.concat([df_stddzed, x_data_todrop], axis=1, join='inner')
     
-	# Predict with the given_model
+    # Predict with the given_model
     pred_label = given_model.predict(x_data)
-    pred_proba = given_model.predict_proba(x_data)
 
-    return pred_label, pred_proba
+    return pred_label
